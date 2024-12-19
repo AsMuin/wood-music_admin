fc76a9dc633921f59f3d4d66e3471aab3cbfac43
AsMuin <asmuins@foxmail.com>
2024-12-20 00:28:32 +0800

    feat: Table 组件

---
+++ src/component/UI/Table.tsx
@@ -0,0 +1,88 @@
+export interface TableColumn<T> {
+    key: keyof T | string;
+
+    header: TableHeader;
+
+    render?: (value: T[keyof T], rowData: T, index: number) => React.ReactNode;
+}
+interface TableConfigProps<T> {
+    columns: TableColumn<T>[];
+    dataSource: T[];
+    isLoading?: boolean;
+    isError?: boolean | string;
+}
+
+function Table<T>({ columns, dataSource, isLoading = false, isError = false }: TableConfigProps<T>) {
+    return (
+        <div className="overflow-x-auto">
+            <table className="table rounded-md border shadow-md">
+                <thead>
+                    <tr>
+                        {columns.map(column => (
+                            <TableHeader key={column.key as string | number} render={column.header} />
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {isLoading ? (
+                        <>
+                            <div className="grid h-full place-content-center">
+                                <span className="loading loading-dots w-32"></span>
+                            </div>
+                        </>
+                    ) : isError ? (
+                        <div className="grid h-full place-content-center">
+                            <span className="text-center text-3xl font-bold text-red-500 drop-shadow-md">请求数据出现了问题</span>
+                        </div>
+                    ) : (
+                        dataSource.map((rowData, index) => <TableRow key={index} columns={columns} rowData={rowData} index={index} />)
+                    )}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+type TableHeader = string | React.ReactNode;
+
+function TableHeader({ render }: { render: TableHeader }) {
+    return <th>{render}</th>;
+}
+
+interface TableRowProps<T> {
+    columns: TableColumn<T>[];
+    rowData: T;
+    index: number;
+}
+function TableRow<T>({ columns, rowData, index }: TableRowProps<T>) {
+    return (
+        <tr className="hover:bg-slate-100">
+            {columns.map(column => (
+                <TableRowCell
+                    key={column.key as string | number}
+                    rowData={rowData}
+                    value={rowData[column.key as keyof T]}
+                    index={index}
+                    render={column.render}
+                />
+            ))}
+        </tr>
+    );
+}
+interface TableRowCellProps<T> {
+    rowData: T;
+    value: T[keyof T];
+    index: number;
+    render?: (value: T[keyof T], rowData: T, index: number) => React.ReactNode;
+    children?: (value: T[keyof T], rowData: T, index: number) => React.ReactNode;
+}
+function TableRowCell<T>({ render, rowData, value, index, children }: TableRowCellProps<T>) {
+    if (children) {
+        return <td>{children(value, rowData, index)}</td>;
+    } else if (render) {
+        return <td>{render(value, rowData, index)}</td>;
+    } else {
+        return <td>{JSON.stringify(value)}</td>;
+    }
+}
+export default Table;
--- src/view/ListSong.tsx
+++ src/view/ListSong.tsx
@@ -1,6 +1,7 @@
 import { showMessage } from '@/component/MessageManager';
 import { IQueryList } from '@/service/api';
 import { getSongList, removeSong } from '@/service/api/song';
+import Table from '@/component/UI/Table';
 import useSWR from 'swr';
 
 export interface ISong {
@@ -23,6 +24,35 @@ function ListSong() {
         getSongList<IQueryList<ISong[]>>({ pageIndex, pageSize })
     );
     const songList = songListResponse?.data?.itemList || [];
+
+    const columns = [
+        {
+            key: 'image',
+            header: '图片',
+            render: (value: string) => <img className="max-h-14 w-12" src={value} alt="" />
+        },
+        {
+            key: 'name',
+            header: '名字'
+        },
+        {
+            key: 'album',
+            header: '专辑'
+        },
+        {
+            key: 'duration',
+            header: '音频时长'
+        },
+        {
+            key: 'action',
+            header: '操作',
+            render: (value: string, rowData: ISong) => (
+                <span className="cursor-pointer text-red-500" onClick={() => onDeleteSong(rowData._id)}>
+                    删除
+                </span>
+            )
+        }
+    ];
     async function onDeleteSong(id: string) {
         try {
             const newSongListResponse = {
@@ -51,48 +81,7 @@ function ListSong() {
         <div>
             <p>所有歌曲列表</p>
             <br />
-            <div>
-                <div className="hidden grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 border border-gray-300 bg-gray-100 p-3 text-sm font-bold text-black sm:grid">
-                    <b>图片</b>
-                    <b>名字</b>
-                    <b>专辑</b>
-                    <b>音频时长</b>
-                    <b>操作</b>
-                </div>
-                <div className="mt-2 grid min-h-96">
-                    {isLoading ? (
-                        <>
-                            <div className="grid h-full place-content-center">
-                                <span className="loading loading-dots w-32"></span>
-                            </div>
-                        </>
-                    ) : error ? (
-                        <div className="grid h-full place-content-center">
-                            <span className="text-center text-3xl font-bold text-red-500 drop-shadow-md">请求数据出现了问题</span>
-                        </div>
-                    ) : songList.length <= 0 ? (
-                        <div className="grid h-full place-content-center">
-                            <span className="text-center text-3xl font-bold text-slate-500 drop-shadow-md">暂无数据</span>
-                        </div>
-                    ) : (
-                        songList.map(song => (
-                            <div
-                                key={song._id}
-                                className="grid grid-cols-[1fr_1fr_1fr] items-center gap-2.5 border border-gray-300 p-3 text-sm sm:grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr]">
-                                <img className="max-h-14 w-12" src={song.image} alt="" />
-                                <p>{song.name}</p>
-                                <p>{song.album}</p>
-                                <p>{song.duration}</p>
-                                <p>
-                                    <span className="cursor-pointer text-red-500" onClick={() => onDeleteSong(song._id)}>
-                                        删除
-                                    </span>
-                                </p>
-                            </div>
-                        ))
-                    )}
-                </div>
-            </div>
+            <Table dataSource={songList} columns={columns} isLoading={isLoading} isError={error} />
         </div>
     );
 }
